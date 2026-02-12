918cfeecc7514d15729fcd40f047a9aa6bb2ab02
Youlik27 <elisey.morozov.27@gmail.com>
2026-02-12 19:50:10 +0100

    1

---
+++ main/static/main/js/quiz.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.answer-btn');
+    const resultModalElement = document.getElementById('resultModal');
+    const resultModal = new bootstrap.Modal(resultModalElement);
+    const resultText = document.getElementById('resultText');
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            const answerId = this.getAttribute('data-id');
+            const checkUrl = this.getAttribute('data-check-url');
+            const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+            fetch(checkUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': csrfToken
+                },
+                body: JSON.stringify({
+                    'answer_id': answerId
+                })
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                resultText.innerText = data.message;
+                resultText.className = "mb-4 fw-bold";
+                if(data.text_class) {
+                    resultText.classList.add(data.text_class);
+                }
+                resultModal.show();
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert("Wystąpił błąd połączenia z serwerem.");
+            });
+        });
+    });
+});
--- quiz/urls.py
+++ quiz/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.view_quiz_menu, name='view_quiz_menu'),
-    path('repeat_and_learn', views.create_quiz_repeat_and_learn, name='create_quiz_repeat_and_learn'),
-
-]
+    path('repeat_and_learn/', views.create_quiz_repeat_and_learn, name='create_quiz_repeat_and_learn'),
+    path('quiz/<int:quiz_id>/<int:question_number>/', views.view_quiz, name='view_quiz'),
+    path('api/check_answer/<int:quiz_id>/<int:question_number>/', views.check_answer, name='check_answer'),
+    ]
