487ecb58575a305199a27915cc23c8bb34dd5018
Youlik27 <elisey.morozov.27@gmail.com>
2026-02-12 19:50:45 +0100

    1

---
+++ quiz/views.py
@@ -0,0 +1,114 @@
+import datetime
+import json
+import random
+from django.http import JsonResponse
+from email._header_value_parser import Word
+from urllib import request
+from core.models import EnglishWord, UserEnglishVocabulary, Quiz, QuizQuestion, WordDefinition
+from django.http import JsonResponse
+
+from django.shortcuts import render, redirect, get_object_or_404
+
+def view_quiz_menu(request):
+    return render(request, 'quiz/quiz_menu.html')
+
+
+def view_quiz(request, quiz_id, question_number=0):
+    quiz = get_object_or_404(Quiz, id=quiz_id)
+    quiz_questions = QuizQuestion.objects.filter(quiz_id=quiz.id).order_by('id')
+    if question_number >= len(quiz_questions):
+        return redirect('quiz_finished')
+    current_question = quiz_questions[question_number]
+    main_word = current_question.word
+    correct_translation = WordDefinition.objects.filter(
+        english_word=main_word
+    ).order_by('-translation_score').first()
+    other_word_ids = quiz_questions.exclude(id=current_question.id).values_list('word_id', flat=True)
+    wrong_defs = []
+    other_words = EnglishWord.objects.filter(id__in=other_word_ids)
+    for word in other_words:
+        top_def = WordDefinition.objects.filter(english_word=word).order_by('-translation_score').first()
+        if top_def:
+            wrong_defs.append(top_def)
+
+    wrong_defs = random.sample(wrong_defs, min(3, len(wrong_defs)))
+    answers = [correct_translation] + wrong_defs
+    random.shuffle(answers)
+    context = {
+        'quiz': quiz,
+        'main_word': main_word,
+        'answers': answers,
+        'correct_translation': correct_translation,
+        'question_number': question_number,
+        'total_questions': len(quiz_questions),
+    }
+    return render(request, 'quiz/view_quiz.html', context)
+def create_quiz_repeat_and_learn(request):
+    user_words = UserEnglishVocabulary.objects.filter(user_id = request.user)
+    user_words_count = user_words.count()
+    number_of_needed_words = 10 - user_words_count
+    user_word_ids = user_words.values_list('word_id', flat=True)
+    if number_of_needed_words > 2:
+        random_words = EnglishWord.objects.exclude(id__in=user_word_ids).order_by('?')[:number_of_needed_words]
+    else:
+        random_words = EnglishWord.objects.exclude(id__in=user_word_ids).order_by('?')[:2]
+    all_questions = list(user_words.values_list('word_id', flat=True)) + list(random_words.values_list('id', flat=True))
+    quiz = Quiz.objects.create(
+        user_id = request.user.id,
+        mode = 'REPEAT_AND_LEARN',
+        total_questions = 10,
+    )
+    for word_id in all_questions:
+        QuizQuestion.objects.create(
+            quiz_id=quiz.id,
+            word_id=word_id,
+            source=request.user
+        )
+
+    print(all_questions)
+    return redirect('view_quiz', quiz_id=quiz.id, question_number=0)
+
+
+def check_answer(request, quiz_id, question_number):
+    data = json.loads(request.body)
+    selected_answer_id = data.get('answer_id')
+
+    quiz = get_object_or_404(Quiz, id=quiz_id)
+    quiz_questions = QuizQuestion.objects.filter(quiz_id=quiz.id).order_by('id')
+
+    if question_number >= len(quiz_questions):
+        return JsonResponse({'error': 'Invalid question number'}, status=400)
+
+    current_question = quiz_questions[question_number]
+    main_word = current_question.word
+
+    selected_def = WordDefinition.objects.filter(id=selected_answer_id).first()
+
+    is_correct = False
+    if selected_def and selected_def.english_word == main_word:
+        is_correct = True
+
+    if is_correct:
+        messages = [
+            "Prawda! To poprawna odpowiedź.",
+            "Brawo! Zgadłeś.",
+            "Świetnie! Dokładnie tak.",
+            "Genialnie! Lecisz dalej."
+        ]
+        text_color = "text-success"
+    else:
+        messages = [
+            "Nieprawda! To błąd.",
+            "Niestety, spróbuj zapamiętać.",
+            "Pudło! Następnym razem się uda.",
+            "Źle! Poprawna odpowiedź była inna."
+        ]
+        text_color = "text-danger"
+
+    random_message = random.choice(messages)
+    return JsonResponse({
+        'is_correct': is_correct,
+        'message': random_message,
+        'text_class': text_color
+    })
+
---
+++ wordlists/views.py
@@ -0,0 +1,84 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
+from core.models import EnglishWord, UserEnglishVocabulary, WordDefinition, WordList, WordListMembership
+from words.views import get_top_definitions_by_pos
+@login_required
+def view_word_lists(request):
+    word_lists = WordList.objects.filter(owner=request.user)
+    return render(request, 'wordlists/word_lists_menu.html', {'word_lists': word_lists})
+
+@login_required
+def create_word_list(request):
+    word_list = WordList.objects.create(
+        name='List ' + timezone.now().strftime("%y%m%d%H%M%S"),
+        owner=request.user
+    )
+    return redirect('word_list_detail', list_id=word_list.id)
+
+def count_words_in_list(word_list_id):
+    return WordListMembership.objects.filter(word_list_id=word_list_id).count()
+
+@login_required
+def word_list_detail(request, list_id):
+    word_list = get_object_or_404(WordList, id=list_id, owner=request.user)
+    memberships = WordListMembership.objects.filter(word_list=word_list)
+    words_data = []
+    for entry in memberships:
+        english_word = entry.word
+        definitions = get_top_definitions_by_pos(english_word)
+        words_data.append({
+            'word': english_word,
+            'definitions': definitions
+        })
+    word_count = count_words_in_list(list_id)
+    return render(request, 'wordlists/manage_word_list.html', {
+        'word_list': word_list, 
+        'words_data': words_data,
+        'word_count': word_count
+    })
+
+@login_required
+def delete_word_list(request, list_id):
+    word_list = get_object_or_404(WordList, id=list_id, owner=request.user)
+    if request.method == 'POST':
+        word_list.delete()
+        messages.success(request, f'Lista "{word_list.name}" została pomyślnie usunięta.')
+        return redirect('word_lists_menu')
+    else:
+        messages.error(request, 'Coś poszło nie так.')
+        return redirect('word_list_detail', list_id=list_id)
+
+@login_required
+def add_word_to_list(request, list_id):
+    word_list = get_object_or_404(WordList, id=list_id, owner=request.user)
+    word_text = request.POST.get('word')
+    word = get_object_or_404(EnglishWord, word=word_text)
+    already_exists = WordListMembership.objects.filter(word=word, word_list=word_list).exists()
+    if not already_exists:
+        WordListMembership.objects.create(
+            word=word,
+            word_list=word_list,
+            added_at=timezone.now()
+        )
+        messages.success(request, f'Słowo "{word.word}" zostało dodane do listy.')
+    else:
+        messages.error(request, 'Słowo jest już zapisane na tej liście.')
+    return redirect('word_list_detail', list_id=list_id)
+
+@login_required
+def remove_word_from_list(request, list_id, word_id):
+    word_list = get_object_or_404(WordList, id=list_id, owner=request.user)
+    word = get_object_or_404(EnglishWord, id=word_id)
+    WordListMembership.objects.filter(word_list=word_list, word=word).delete()
+    return redirect('word_list_detail', list_id=word_list.id)
+
+
+def update_word_list_name(request, list_id):
+    new_name = request.POST.get('new_name')
+    word_list = get_object_or_404(WordList, id=list_id, owner=request.user)
+    word_list.name = new_name
+    word_list.save()
+    return redirect('word_list_detail', list_id=list_id)
+
